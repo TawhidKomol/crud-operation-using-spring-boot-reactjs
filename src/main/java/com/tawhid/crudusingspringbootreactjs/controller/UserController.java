90d408697c95e66cbeb12af64f6fa5d3627ceb77
Tawhid-Komol <tawhid15-2861@diu.edu.bd>
2023-02-18 23:53:56 +0600

    update

--- src/main/java/com/tawhid/crudusingspringbootreactjs/controller/UserController.java
+++ src/main/java/com/tawhid/crudusingspringbootreactjs/controller/UserController.java
@@ -3,14 +3,12 @@ package com.tawhid.crudusingspringbootreactjs.controller;
 import com.tawhid.crudusingspringbootreactjs.model.User;
 import com.tawhid.crudusingspringbootreactjs.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
